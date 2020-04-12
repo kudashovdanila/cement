1d66db27565a1fda4888fe8b5d1b865fc23b650c
kudashovdanila <48285878+kudashovdanila@users.noreply.github.com>
2020-04-12 23:42:44 +0700

    Секция о компании

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.hero-slider').slick({
+    arrows: false,
+    fade: true,
+    cssEase: 'linear',
+<<<<<<< HEAD
+    // autoplay: true,
+=======
+    autoplay: true,
+>>>>>>> d5e7fe8b4275c6be8008a1c20f10e2316d6082fb
+    autoplaySpeed: 5000,
+    dots: true
+  });
+});
