606310387dc37d9f30da45d64db9c47a19644022
kudashovdanila <48285878+kudashovdanila@users.noreply.github.com>
2020-04-13 09:03:19 +0700

    Сверстал о компании

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@ $(document).ready(function () {
     arrows: false,
     fade: true,
     cssEase: 'linear',
-<<<<<<< HEAD
     // autoplay: true,
-=======
-    autoplay: true,
->>>>>>> d5e7fe8b4275c6be8008a1c20f10e2316d6082fb
     autoplaySpeed: 5000,
     dots: true
   });
